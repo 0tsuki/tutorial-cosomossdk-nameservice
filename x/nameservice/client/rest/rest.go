211e26506ef531bda84b35aed5f48672a270cfa3
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2020-06-04 16:24:51 +0900

    step16: imple Query Handlers

--- x/nameservice/client/rest/rest.go
+++ x/nameservice/client/rest/rest.go
@@ -1,13 +1,19 @@
 package rest
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 )
 
+const (
+	restName = "name"
+)
+
 // RegisterRoutes registers nameservice-related REST handlers to a router
-func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	registerQueryRoutes(cliCtx, r)
-	registerTxRoutes(cliCtx, r)
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) {
+	r.HandleFunc(fmt.Sprintf("/%s/names", storeName), namesHandler(cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/names/{%s}", storeName, restName), resolveNameHandler(cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/names/{%s}/whois", storeName, restName), whoisHandler(cliCtx, storeName)).Methods("GET")
 }
